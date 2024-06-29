ef95a7e9afb67b2e38ca3ee9420bf9cac07e5b35
Korjubzot <korjubzot@gmail.com>
2024-06-29 09:32:55 +0200

    Cleaned up some of the music player logic

--- src/components/MusicPlayer/MusicPlayer.js
+++ src/components/MusicPlayer/MusicPlayer.js
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from "react";
 import useSound from "use-sound";
-import { Howl, Howler } from "howler";
 // TODO migrate this component to HowlerJS for improved usability
 
-import song1 from "../../assets/music/song1.mp3";
-import song2 from "../../assets/music/song2.mp3";
-import song3 from "../../assets/music/song3.mp3";
-import song4 from "../../assets/music/song4.mp3";
-import song5 from "../../assets/music/song5.mp3";
-import song6 from "../../assets/music/song6.mp3";
-import song7 from "../../assets/music/song7.mp3";
-import song8 from "../../assets/music/song8.mp3";
-import song9 from "../../assets/music/song9.mp3";
-// This is almost certainly the worst possible way to do this
-// I'm doing it anyways!
-// TODO make this less terrible
-
 function MusicPlayer() {
-  const songs = [song1, song2, song3, song4, song5, song6, song7, song8, song9];
+  const songs = [
+    { src: require("../../assets/music/song1.mp3"), title: "Song 1" },
+    { src: require("../../assets/music/song2.mp3"), title: "Song 2" },
+    { src: require("../../assets/music/song3.mp3"), title: "Song 3" },
+    { src: require("../../assets/music/song4.mp3"), title: "Song 4" },
+    { src: require("../../assets/music/song5.mp3"), title: "Song 5" },
+    { src: require("../../assets/music/song6.mp3"), title: "Song 6" },
+    { src: require("../../assets/music/song7.mp3"), title: "Song 7" },
+    { src: require("../../assets/music/song8.mp3"), title: "Song 8" },
+    { src: require("../../assets/music/song9.mp3"), title: "Song 9" },
+  ];
+  // I'd like to think that a professional would never do this, but I know for a fact
+  // that Team Fortress 2 does
+
   const [currentSongIndex, setCurrentSongIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [play, { stop }] = useSound(songs[currentSongIndex], { volume: 1 });
+  const [play, { stop }] = useSound(songs[currentSongIndex].src, { volume: 1 });
 
   useEffect(() => {
     if (isPlaying) {
@@ -29,11 +28,6 @@ function MusicPlayer() {
       stop();
     }
   }, [isPlaying, play, stop]);
-
-  // TODO Fix a bug in these handlers that's causing them to stack songs
-  // on top of one another rather than skip forward properly
-  // Probably just need to add a stop() call to the handlers
-  // before changing the track
 
   function trackForwardHandler() {
     console.log("Track forward");
@@ -55,7 +49,9 @@ function MusicPlayer() {
     <div>
       <div>
         <h1 className="text-center">Music Player</h1>
-        <h2 className="text-center">Now Playing: {songs[currentSongIndex]}</h2>
+        <h2 className="text-center">
+          Now Playing: {songs[currentSongIndex].title}
+        </h2>
       </div>
       <div className="flex flex-row justify-center">
         <button className="btn" onClick={trackBackwardHandler}>
